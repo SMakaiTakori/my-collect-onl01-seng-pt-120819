72fa1173fd9a8ec308621e9a5229bca49fce26fa
S. Makai Takori <makait87@gmail.com>
2019-12-18 05:51:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,7 +5,7 @@ def my_collect(array)
    if array.length == 0
       return
    end
-  if array.split(" ").count > 1 
+  if array[0].split(" ").count > 1 
    while i < array.length 
     collection << yield(array[i].split(" ").first) 
     i += 1
