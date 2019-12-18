31f024217a60af9217c6f3f1f0f4dd8f2f1c612c
S. Makai Takori <makait87@gmail.com>
2019-12-18 05:33:33 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
-
+def my_collect(array)
+   i = 0
+   collection = []
+   
+  if array.split(" ").count > 1 do |name|
+    name.split(" ").first    
+  else
+    
+    
+    
+  
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  
+end
+ 
+  
+end  
 
