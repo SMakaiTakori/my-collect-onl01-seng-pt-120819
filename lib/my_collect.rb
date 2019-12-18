def10b5af86e5efd0aaa53618123835ce326419c
S. Makai Takori <makait87@gmail.com>
2019-12-18 05:48:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
    i = 0
    collection = []
    
-  if array.split(" ").count > 1 
+  if array[0].split(" ").count > 1 
    while i < array.length 
     collection << yield(array[i].split(" ").first) 
     i += 1
